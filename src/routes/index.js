47b833a9d476c01db76203a19d08fcb72edc091f
Marc <>
2022-08-18 15:51:07 +0800

    改路由

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import SettingForm from '../pages/SettingForm.vue'
 import Trace from '../pages/Trace.vue'
 const routes = [
-  { path: '/', name: 'Setting', component: SettingForm },
-  { path: '/trace', name: 'Trace', component: Trace },
+  { path: '/', name: 'Setting', component: Trace }
+  // { path: '/trace', name: 'Trace', component: Trace },
 ]
 
 export default createRouter({
   history: createWebHistory(),
-  routes,
+  routes
 })
